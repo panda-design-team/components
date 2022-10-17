05e791de1be5ffc4be14bd5cfb68c10aa28179a5
Cong Zhang <dancerphil1994@gmail.com>
2022-10-17 10:58:17 +0800

    0.0.13

--- src/button/index.tsx
+++ src/button/index.tsx
@@ -22,7 +22,6 @@ const Button = React.forwardRef<unknown, ButtonProps>(({tooltip, disabledReason,
     ].filter(Boolean).join(' ');
     const nextProps = {
         ...props,
-        prefixCls: '_',
         type: 'custom',
         icon: nextIcon,
         size: undefined,
